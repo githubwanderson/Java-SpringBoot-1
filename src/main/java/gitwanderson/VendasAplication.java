7cc4a5fa142997f9780264ec43314130c7d63a33
Wanderson <emailwandersonalves@gmail.com>
2023-12-28 10:58:57 -0300

    Hello world

--- src/main/java/gitwanderson/VendasAplication.java
+++ src/main/java/gitwanderson/VendasAplication.java
@@ -2,9 +2,18 @@ package gitwanderson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasAplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello world";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasAplication.class, args);
     }
