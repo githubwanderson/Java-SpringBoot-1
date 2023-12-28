707b524e9767dd5e8dbae708948c3947ecf2bf5a
Wanderson <emailwandersonalves@gmail.com>
2023-12-28 12:46:40 -0300

    Add Injecao de dependencia com construtor

--- src/main/java/gitwanderson/service/ClienteService.java
+++ src/main/java/gitwanderson/service/ClienteService.java
@@ -2,15 +2,22 @@ package gitwanderson.service;
 
 import gitwanderson.entity.Cliente;
 import gitwanderson.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
 
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteService( ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+
     public void salvarCliente(Cliente cliente){
         validarCliente(cliente);
-        ClienteRepository clienteRepository = new ClienteRepository();
-        clienteRepository.save(cliente);
+        this.clienteRepository.save(cliente);
     }
 
     private void validarCliente(Cliente cliente) {
