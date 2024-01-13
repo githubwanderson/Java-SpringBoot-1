2453a5634e97f9a4666f3f0e541abd14d380b45e
Wanderson <emailwandersonalves@gmail.com>
2024-01-13 15:01:29 -0300

    aula 11 application.properties

--- src/main/java/gitwanderson/VendasAplication.java
+++ src/main/java/gitwanderson/VendasAplication.java
@@ -2,6 +2,7 @@ package gitwanderson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,16 @@ public class VendasAplication {
     @Qualifier("applicationName")
     private String applicationName;
 
+//  Esta pegando os valores de resource->application.properties
+    @Value("${application.name}")
+    private String status;
+    
     @GetMapping("/hello")
     public String helloWorld() {
-        return applicationName;
+        return applicationName + ' ' + status;
     }
 
+//  Digite psvm para auto completar
     public static void main(String[] args) {
         SpringApplication.run(VendasAplication.class, args);
     }
--- src/main/java/gitwanderson/service/ClienteService.java
+++ src/main/java/gitwanderson/service/ClienteService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteService {
 
+//  Cliente service vai precisar da classe ClienteRepository
     @Autowired
     private ClienteRepository clienteRepository;
 
