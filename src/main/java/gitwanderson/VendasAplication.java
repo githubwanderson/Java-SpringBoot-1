b52b1b15fca94f2d8badf96db42ab523ce3dd7f4
Wanderson <emailwandersonalves@gmail.com>
2024-01-14 19:54:06 -0300

    Aula 19 Salvando e listando Clientes

--- src/main/java/gitwanderson/VendasAplication.java
+++ src/main/java/gitwanderson/VendasAplication.java
@@ -1,12 +1,18 @@
 package gitwanderson;
 
+import gitwanderson.entity.Cliente;
+import gitwanderson.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -24,6 +30,21 @@ public class VendasAplication {
     @GetMapping("/hello")
     public String helloWorld() {
         return applicationName;
+    }
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClienteRepository clienteRepository){
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Wanderson");
+            clienteRepository.save(cliente);
+
+//          usando o contrutor
+            clienteRepository.save(new Cliente("Lucas"));
+
+            List<Cliente> listClientes = clienteRepository.listar();
+            listClientes.forEach(System.out::println);
+        };
     }
 
 //  Digite psvm para auto completar
