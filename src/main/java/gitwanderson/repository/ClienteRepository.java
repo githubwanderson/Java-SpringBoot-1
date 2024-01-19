3c254f9145a3e3a22fcf625d03c9c23e34d5351e
Wanderson <emailwandersonalves@gmail.com>
2024-01-19 08:06:23 -0300

    aula 23 refatorando demais metodos clienteRepository para JPA

--- src/main/java/gitwanderson/repository/ClienteRepository.java
+++ src/main/java/gitwanderson/repository/ClienteRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -35,38 +36,40 @@ public class ClienteRepository {
         return cliente;
     }
 
+    @Transactional
     public Cliente update(Cliente cliente){
-        jdbcTemplate.update( UPDATE, new Object[]{cliente.getNome(),cliente.getId()});
+        entityManager.merge(cliente);
         return cliente;
     }
 
+    @Transactional
     public void delete(Cliente cliente){
-        deleteById(cliente.getId());
+        if(!entityManager.contains(cliente)){
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
     }
 
+    @Transactional
     public void deleteById(Integer id){
-        jdbcTemplate.update(DELETE, new Object[]{id});
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        delete(cliente);
     }
 
-//    public Cliente getById(Integer id){
-//        return jdbcTemplate.update(SELECT, new Object[]{id}, getClienteMapper());
-//    }
-
+    @Transactional(readOnly = true)
     public List<Cliente> listarByName(String nome){
-        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "), new Object[]{"%" + nome + "%"}, getClienteMapper());
+        // Select referenciando a classe
+        String jpql = " select c from Cliente c where c.nome like :nome ";
+        // Passo a instrução e a classe referenciada que vai me retornar um typedquery (query tipada para entidade Cliente)
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome","%" + nome + "%");
+        return query.getResultList();
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> listar(){
-        return jdbcTemplate.query(SELECT_ALL, getClienteMapper());
-    }
-
-    // Criando um metodo a partir de uma seleção Ctrl + Alt + m
-    private RowMapper<Cliente> getClienteMapper() {
-        return new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                return new Cliente(resultSet.getInt("id"),resultSet.getString("nome"));
-            }
-        };
+        return entityManager
+                .createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
