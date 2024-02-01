1bf106d481a0d3d285028fdb044299b7b0380675
Wanderson <emailwandersonalves@gmail.com>
2024-01-31 21:43:42 -0300

    aula 29 mapeando a entidade pedido

--- src/main/java/gitwanderson/entity/Pedido.java
+++ src/main/java/gitwanderson/entity/Pedido.java
@@ -1,11 +1,24 @@
 package gitwanderson.entity;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pedido")
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "total")
     private Float total;
+
+    @Column(name = "data_pedido")
     private LocalDate data;
 
     public Integer getId() {
