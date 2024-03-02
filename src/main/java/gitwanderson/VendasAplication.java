93a447cfe2ffc99d9e8714a0187680736fbadf39
Wanderson <emailwandersonalves@gmail.com>
2024-03-02 13:09:31 -0300

    aula 38 request GET

--- src/main/java/gitwanderson/VendasAplication.java
+++ src/main/java/gitwanderson/VendasAplication.java
@@ -1,10 +1,25 @@
 package gitwanderson;
 
+import gitwanderson.domain.entity.Cliente;
+import gitwanderson.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasAplication {
+
+    @Bean // Para reconhecer como um comando gerenciavel
+    public CommandLineRunner init(
+            @Autowired ClienteRepository clienteRepository
+            ){
+        return args -> {
+            Cliente c = new Cliente("Wandinelson");
+            clienteRepository.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasAplication.class, args);
--- src/main/java/gitwanderson/rest/controller/ClienteController.java
+++ src/main/java/gitwanderson/rest/controller/ClienteController.java
@@ -1,19 +1,31 @@
 package gitwanderson.rest.controller;
 
+import gitwanderson.domain.entity.Cliente;
+import gitwanderson.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
-@RequestMapping("/api/clientes")
+@RequestMapping("/api")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
-    @ResponseBody // Informa que a string de retorno sera resposta da requisição
-    public String helloCliente ( @PathVariable("nome") String nomeClient ){
-        return String.format("hello %s", nomeClient);
-    }
+    @Autowired
+    private ClienteRepository clienteRepository;
 
+    @RequestMapping(value = "/clientes/{id}", method = RequestMethod.GET)
+    @ResponseBody // Informa que a string de retorno sera resposta da requisição
+    public ResponseEntity getClienteById (@PathVariable("id") Integer id ){
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get()); // Devolvo 200 e o cliente
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
