afc16a6df3a8b35545d44412e9160d304a2891dd
Wanderson <emailwandersonalves@gmail.com>
2024-03-06 08:30:11 -0300

    aula 42 request PARAM LIST

--- src/main/java/gitwanderson/domain/entity/Cliente.java
+++ src/main/java/gitwanderson/domain/entity/Cliente.java
@@ -14,8 +14,11 @@ public class Cliente {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "nome")
+    @Column(name = "nome", length = 100)
     private String nome;
+
+    @Column(name = "cpf", length = 11)
+    private String cpf;
 
     public Set<Pedido> getPedidos() {
         return pedidos;
@@ -63,6 +66,14 @@ public class Cliente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     @Override
--- src/main/java/gitwanderson/rest/controller/ClienteController.java
+++ src/main/java/gitwanderson/rest/controller/ClienteController.java
@@ -4,10 +4,13 @@ import gitwanderson.domain.entity.Cliente;
 import gitwanderson.domain.repository.ClienteRepository;
 import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -62,8 +65,41 @@ public class ClienteController {
                     } ).orElseGet( () -> ResponseEntity.notFound().build() );
     }
 
+    // aula 42
+    // Listando clientes
+    @RequestMapping(value = "/clientes", method = RequestMethod.GET)
+    public ResponseEntity find( Cliente filtro ){
 
+        // Modo manual
+        // verificar o que veio no filtro para então montar o select para buscar no banco
+        /*
+        String sql = "select * from cliente ";
 
+        if(filtro.getNome() != null){
+            sql += " where nome = :nome ";
+        }
 
+        if(filtro.getCpf() != null){
+            sql += " where cpf = :cpf ";
+        }
+        */
 
+        // recurso que faz essa validacao
+
+        // parametro para o example onde definimos as configuracoes
+            // .withIgnoreCase() // Ignore maiuscula e minuscula para buscar
+            // .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING ) // busca a string que CONTEM
+        ExampleMatcher matcher = ExampleMatcher
+                                        .matching()
+                                        .withIgnoreCase()
+                                        .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING );
+
+        // example recebe uma classe onde ele vai procurar as propriedades
+        // pacote data.domain
+        Example example = Example.of(filtro, matcher);
+
+        List<Cliente> lista = clienteRepository.findAll( example );
+
+        return ResponseEntity.ok(lista);
+    }
 }
