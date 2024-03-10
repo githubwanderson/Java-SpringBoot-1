1c8a118f0aad11510b492021b9df40f16c74de08
Wanderson <emailwandersonalves@gmail.com>
2024-03-10 10:55:12 -0300

    aula 43 refatorando ClienteController para RestController

--- src/main/java/gitwanderson/rest/controller/ClienteController.java
+++ src/main/java/gitwanderson/rest/controller/ClienteController.java
@@ -2,77 +2,91 @@ package gitwanderson.rest.controller;
 
 import gitwanderson.domain.entity.Cliente;
 import gitwanderson.domain.repository.ClienteRepository;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
-@Controller
-@RequestMapping("/api")
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @RequestMapping(value = "/clientes/{id}", method = RequestMethod.GET)
-    @ResponseBody // Informa que a string de retorno sera resposta da requisição
-    public ResponseEntity getClienteById (@PathVariable("id") Integer id ){
+    @GetMapping(value = "/{id}")
+    public Cliente getCliente(@PathVariable("id") Integer id) {
+
+        /*
+
+        Opção 1
+
         Optional<Cliente> cliente = clienteRepository.findById(id);
         if(cliente.isPresent()){
-            return ResponseEntity.ok(cliente.get()); // Devolvo 200 e o cliente
+            return cliente.get(); // Devolvo 200 e o cliente
         }
-        return ResponseEntity.notFound().build();
-    }
+        throw new ResponseStatusException( HttpStatus.NOT_FOUND, "Cliente não encontrado." );
 
-    @RequestMapping(value = "/cliente", method = RequestMethod.POST)
-    @ResponseBody
-    public ResponseEntity saveCliente ( @RequestBody Cliente cliente ){
-        Cliente c = clienteRepository.save(cliente);
-        return ResponseEntity.ok(c);
-    }
+        Opção 2 otimizando
 
-    @RequestMapping(value = "/cliente/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public ResponseEntity deleteCliente ( @PathVariable("id") Integer id ){
-        Optional<Cliente> c = clienteRepository.findById(id);
-
-        if(c.isPresent()){
-            clienteRepository.deleteById(id);
-            return ResponseEntity.noContent().build(); // retorna 204 no content
-        }
-
-        return ResponseEntity.notFound().build();
-    }
-
-    // PUT atualiza integralmente a entity
-    // Se passar algum campo null então deve ser salvo null
-    @RequestMapping(value = "/cliente/{id}", method = RequestMethod.PUT )
-    @ResponseBody
-    public ResponseEntity putCliente( @PathVariable("id") Integer id, @RequestBody Cliente cliente ){
+        */
 
         return clienteRepository
-                    .findById(id)
-                    .map( clienteFind -> {
-                        cliente.setId(clienteFind.getId());
-                        clienteRepository.save(cliente);
-                        return ResponseEntity.noContent().build();
-                    } ).orElseGet( () -> ResponseEntity.notFound().build() );
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado.")
+                );
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    // Retorno padrão é 200 mas se quiser retornar um status diferente então deve se colocar responsestatus
+    public Cliente saveCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCliente(@PathVariable Integer id) {
+
+        clienteRepository.findById( id )
+                .map( cliente -> {
+                    clienteRepository.delete( cliente );
+                    return cliente;
+                })
+                .orElseThrow( () ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado.")
+                );
+    }
+
+    // PUT atualiza integralmente a entity // Se passar algum campo null ou não passar todas as propriedades então sera salvo null
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCliente(@PathVariable("id") Integer id, @RequestBody Cliente cliente) {
+        clienteRepository
+                .findById(id)
+                .map(clienteFind -> {
+                    cliente.setId(clienteFind.getId());
+                    clienteRepository.save(cliente);
+                    return clienteFind;
+                }).orElseThrow(() ->
+                    new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado")
+                );
     }
 
     // aula 42
     // Listando clientes
-    @RequestMapping(value = "/clientes", method = RequestMethod.GET)
-    public ResponseEntity find( Cliente filtro ){
+    @GetMapping
+    public List<Cliente> find(Cliente filtro) {
 
+        /*
         // Modo manual
         // verificar o que veio no filtro para então montar o select para buscar no banco
-        /*
+
         String sql = "select * from cliente ";
 
         if(filtro.getNome() != null){
@@ -84,22 +98,17 @@ public class ClienteController {
         }
         */
 
-        // recurso que faz essa validacao
-
         // parametro para o example onde definimos as configuracoes
-            // .withIgnoreCase() // Ignore maiuscula e minuscula para buscar
-            // .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING ) // busca a string que CONTEM
+        // .withIgnoreCase() // Ignore maiuscula e minuscula para buscar
+        // .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING ) // busca a string que CONTEM
         ExampleMatcher matcher = ExampleMatcher
-                                        .matching()
-                                        .withIgnoreCase()
-                                        .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING );
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
 
         // example recebe uma classe onde ele vai procurar as propriedades
         // pacote data.domain
         Example example = Example.of(filtro, matcher);
-
-        List<Cliente> lista = clienteRepository.findAll( example );
-
-        return ResponseEntity.ok(lista);
+        return clienteRepository.findAll(example);
     }
 }
