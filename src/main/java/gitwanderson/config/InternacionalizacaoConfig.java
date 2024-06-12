4541a7263be139c2b8d2f5c9dc68cd5adcd3717e
Wanderson <emailwandersonalves@gmail.com>
2024-06-12 11:01:51 -0300

    aula 65 - Internacionalizacao - criando clÃasse para ler e fazer e substituir as mensagens

--- src/main/java/gitwanderson/config/InternacionalizacaoConfig.java
+++ src/main/java/gitwanderson/config/InternacionalizacaoConfig.java
@@ -1,4 +1,32 @@
 package gitwanderson.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
+@Configuration
 public class InternacionalizacaoConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        // Referenciando o arquivo .properties que contem as mensagens
+        messageSource.setBasename("classpath:messages");
+        // Informo a codificação
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
+    // Responsavel por add as mensagens nos arquivos
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
 }
